478816c68cf3b319422176bdfd94a98d216ff79a
Gautham2980 <gautham4816@gmail.com>
2023-10-27 18:17:56 +0530

    task2

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let a=prompt("enter the first number");
+let b=prompt("enter the second number");
+
+let division=(x,y,divide)=>{
+    divide(x,y)
+}
+
+divide=(x,y)=>{
+    let c=x/y;
+    document.write(`Divide two number is ${c}`)
+
+}
+
+division(a,b,divide)
+
+
+
+
+
+
